c614ff74350f5b3638594c51095a54a5e73a4f6f
Luana Alves Nogueira <luanagcontato@gmail.com>
2023-10-20 19:33:47 -0300

    System Structure

---
+++ src/main/java/com/connect/system/ConnectSystemApplication.java
@@ -0,0 +1,13 @@
+package com.connect.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/connect/system/ConnectSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connect.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
